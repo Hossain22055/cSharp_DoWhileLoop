abf170c9f3b8a43b8aae7f7ce5d3a8d917790333
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-01-29 11:51:21 -0500

    This is Done

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Learning Do While Loop
+            
+
+            // Example #1 
+            //This is where the condition is false so hello will be only print once
+            var i = 0;
+            do
+            {
+                Console.WriteLine("Hello");
+                i++;
+            } while (i > 5);
+            Console.WriteLine();
+
+            //Example #2 
+            // This will print 5 to 1 
+            var m = 5;
+            do
+            {
+                Console.WriteLine(m);
+                m--;
+            } while (m > 0);
+            Console.WriteLine();
+
+
+            //Example #3 
+            //Print 3 to 0
+            int n = 3;
+            do
+            {
+                Console.WriteLine(n);
+                n--;
+            } while (n >= 0);
+            Console.WriteLine();
+
+
+            //Example #4
+            int h = 7;
+            do
+            {
+                Console.WriteLine("Done learning DO WHILE loop");
+                h--;
+            } while (h >= 5);
+
+        }
+       
+    }
+}
